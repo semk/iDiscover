8220937b1503a159e6f7ce8e7fd5507ad4c979e2
Sreejith K <sreejithemk@gmail.com>
2014-05-23 20:19:31 +0530

    * Fix the setup script

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     download_url='https://github.com/semk/iDiscover/tarball/master',
     install_requires=['ipcalc >= 1.1.2'],
     packages=find_packages(),
-    data_files=['data/oui.txt'],
+    data_files=[('data/oui.txt', ['data/oui.txt'])],
     include_package_data=True,
     entry_points={
         'console_scripts': ['idiscover = idiscover.discover:run']
